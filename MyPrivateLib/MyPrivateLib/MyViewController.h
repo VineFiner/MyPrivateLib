fab2ae91b38f602f05e82e26dc09f38cda6dd849
Vine <1953892532@qq.com>
2017-01-01 22:34:03 +0800

    first commit

---
+++ MyPrivateLib/MyPrivateLib/MyViewController.h
@@ -0,0 +1,13 @@
+//
+//  MyViewController.h
+//  MyPrivateLib
+//
+//  Created by mac on 17/1/1.
+//  Copyright © 2017年 SunQL. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MyViewController : UIViewController
+
+@end
---
+++ MyPrivateLib/MyPrivateLib/MyViewController.m
@@ -0,0 +1,38 @@
+//
+//  MyViewController.m
+//  MyPrivateLib
+//
+//  Created by mac on 17/1/1.
+//  Copyright © 2017年 SunQL. All rights reserved.
+//
+
+#import "MyViewController.h"
+
+@interface MyViewController ()
+
+@end
+
+@implementation MyViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.view.backgroundColor = [UIColor orangeColor];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
